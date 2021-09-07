cc80e3da8f865a4cec424eca4a315b5c04dbf0fd
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-09-07 13:06:40 -0300

    API retorna dados de cidades individualmente e de todas as cidades

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,13 @@
 import coordenadas as cd
 
+#Busca os dados de cidade especifica
 def buscar_cidade(cidade):
     for i in cd.coordenadas:
         if i['cidade'] == cidade:
             return i
-    return False    
+    return False
+
+#Busca os dados de todas as cidades
+def listar_todas_cidades():
+    cidades = cd.coordenadas
+    return cidades
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 from funcoes import *
 
 app = Flask(__name__)
 api = Api(app)
 
+#CLASSE PARA COORDENADAS
 class Coordenadas(Resource):
 
+    #Busca os dados de uma cidade especifica
     def get(self, cidade):
         try:
             resul = buscar_cidade(cidade.upper())
@@ -24,7 +26,28 @@ class Coordenadas(Resource):
                 }
             return response
 
-api.add_resource(Coordenadas, '/valedoparaiba/coordenadas/<string:cidade>')
+#CLASSE PARA CIDADES
+class Cidades(Resource):
+
+    #Busca os dados de todas as cidades
+    def get(self):
+        try:
+            resul = listar_todas_cidades()
+            return resul
+        except:
+            response = {
+                    "erro":"Erro ao tentar consultar coordenada da cidade."
+                }
+            return response
+
+#Retorna os links para acesso a raiz da API
+@app.route('/')
+def pagina_inicial():
+    return render_template('inicio.html')
+
+#REGISTRO DAS ROTAS
+api.add_resource(Coordenadas, '/valedoparaiba/coordenadas/<string:cidade>/')
+api.add_resource(Cidades, '/valedoparaiba/')
 
 if __name__ == '__main__':
     app.run()
